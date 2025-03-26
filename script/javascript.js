201d9c4475aed652337ba436a93937a8e614f9a6
Claudia Walker <claudia.walker@iu-study.org>
2025-03-26 18:17:22 +0100

    Top-Button mit Funktion eingefügt

--- script/javascript.js
+++ script/javascript.js
@@ -1,2 +1,15 @@
-document.documentElement.classList.remove('no-js'); 
-document.documentElement.classList.add('js'); 
+document.documentElement.classList.remove('no-js');
+document.documentElement.classList.add('js');
+
+window.onscroll = function() {
+    const scrollTopButton = document.getElementById("scrollTopButton");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      scrollTopButton.style.display = "block";
+    } else {
+      scrollTopButton.style.display = "none";
+    }
+  };
+
+  function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
