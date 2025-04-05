3040bd2a6291f13c2185388433f0ba249fb78857
Claudia Walker <claudia.walker@iu-study.org>
2025-04-05 11:43:10 +0200

    href-Link im head angepasst

--- script/javascript.js
+++ script/javascript.js
@@ -2,29 +2,29 @@ document.documentElement.classList.remove('no-js');
 document.documentElement.classList.add('js');
 
 window.onscroll = function() {
-    const scrollTopButton = document.getElementById("scrollTopButton");
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      scrollTopButton.style.display = "block";
-    } else {
-      scrollTopButton.style.display = "none";
-    }
-  };
+  const scrollTopButton = document.getElementById("scrollTopButton");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollTopButton.style.display = "block";
+  } else {
+    scrollTopButton.style.display = "none";
+  }
+}
 
   // Nach oben springen
-  function scrollToTop() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  }
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
 
   // Sprachenwechsel
-  function switchLanguage() {
-    var languageSwitcher = document.getElementById('language-switcher');
-    var selectedValue = languageSwitcher.value;
-    var currentPath = window.location.pathname;
+function switchLanguage() {
+  var languageSwitcher = document.getElementById('language-switcher');
+  var selectedValue = languageSwitcher.value;
+  var currentPath = window.location.pathname;
 
-    // Replace the current language part of the URL with the selected language
-    if (currentPath.includes('_en')) {
-        window.location.href = currentPath.replace('_en', '');
-    } else {
-        window.location.href = currentPath.replace('.html', '_en.html');
-    }
+  // Replace the current language part of the URL with the selected language
+  if (currentPath.includes('_en')) {
+      window.location.href = currentPath.replace('_en', '');
+  } else {
+      window.location.href = currentPath.replace('.html', '_en.html');
+  }
 }
