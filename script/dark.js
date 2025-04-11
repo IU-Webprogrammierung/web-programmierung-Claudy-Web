3498abe767746ccaee3540da0056896183302bdd
Claudia Walker <claudia.walker@iu-study.org>
2025-04-11 18:30:45 +0200

    Fehler bei Pfade im Skript dark.js behoben

--- script/dark.js
+++ script/dark.js
@@ -8,16 +8,16 @@ window.addEventListener("DOMContentLoaded", () => {
   // Fall 1: gespeicherte Einstellung
   if (savedTheme === "dark") {
     document.body.classList.add("dark-mode");
-    icon.src = "../pictures/sun.svg";
+    icon.src = "pictures/sun.svg";
   } else if (savedTheme === "light") {
     document.body.classList.remove("dark-mode");
-    icon.src = "../pictures/moon.svg";
+    icon.src = "pictures/moon.svg";
   } else if (prefersDark) {
     // Fall 2: keine gespeicherte Einstellung → Systemvorgabe übernehmen
     document.body.classList.add("dark-mode");
-    icon.src = "../pictures/sun.svg";
+    icon.src = "pictures/sun.svg";
   } else {
-    icon.src = "../pictures/moon.svg";
+    icon.src = "pictures/moon.svg";
   }
 });
 
@@ -26,5 +26,5 @@ function toggleDarkMode() {
   const icon = document.getElementById("darkModeIcon");
   const isDark = document.body.classList.toggle("dark-mode");
   localStorage.setItem("theme", isDark ? "dark" : "light");
-  icon.src = isDark ? "../pictures/sun.svg" : "../pictures/moon.svg";
+  icon.src = isDark ? "pictures/sun.svg" : "pictures/moon.svg";
 }
